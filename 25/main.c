f0b140cea950c5e2f6092ce0d6cfbb91cccb1496
brian <B06504102@ntu.edu.tw>
2020-03-30 02:48:35 +0800

    group1

--- 25/main.c
+++ 25/main.c
@@ -20,15 +20,6 @@ typedef struct {
     struct doublelinklist *cursor;
 } Info;
 
-Info *genInfo(){
-    Info *I=(Info*)malloc(sizeof(Info));
-    I->head=NULL;
-    I->tail=NULL;
-    I->cursor=NULL;
-    return I;
-}
-
-
 DoubleLL *genDoubleLL(char letter){
     DoubleLL *DLL = (DoubleLL*)malloc(sizeof(DoubleLL));
     DLL->letter=letter;
@@ -37,30 +28,28 @@ DoubleLL *genDoubleLL(char letter){
     return DLL;
 }
 
+Info *genInfo(){
+    Info *I=(Info*)malloc(sizeof(Info));
+    DoubleLL *Begin = genDoubleLL('S');//最左邊設一個不存在的位置,方便cursor指
+    I->head=Begin;
+    I->tail=Begin;
+    I->cursor=Begin;
+    return I;
+}
+
 Info *Insert(DoubleLL *new , Info *info){
 
-    if(info->tail==NULL && info->head==NULL && info->cursor==NULL){//text全空
-        info->head=new;
-        info->tail=new;
-        info->cursor=new;
-    }else if(info->cursor==NULL){//text有東西,info->cursor在最頭
-        new->next=info->head;
-        info->head->prev=new;
-        info->head=new;
-        info->cursor=new;
-    }else if(info->cursor==info->tail){//text有東西,info->cursor在最尾
-        new->prev=info->tail;
-        info->tail->next=new;
-        info->tail=new;
-        info->cursor=new;
-    }else{//text有東西,info->cursor在中間
-
-    DoubleLL *temp;
-    temp=info->cursor->next;//把下一個存起來
-    info->cursor->next=new;//插入
-    new->next=temp;
-    new->prev=info->cursor;
-    info->cursor=new;
+    if(info->cursor == info->tail){//cursor在最尾
+        new->prev = info->tail;
+        info->tail->next = new;
+        info->tail = new;
+        info->cursor = new;
+    }else{
+        new->prev = info->cursor;
+        new->next = info->cursor->next;
+        new->next->prev = new;
+        info->cursor->next = new;
+        info->cursor = new;
     }
 
     return info;
@@ -68,16 +57,30 @@ Info *Insert(DoubleLL *new , Info *info){
 
 Info *Overwrite(DoubleLL *new , DoubleLL *selectStart , Info *info , int select){
     if(select>0){//selectStart -> new -> cursor的next
-        new->next = info->cursor->next;
-        new->next->prev = new;
         selectStart->next = new;
         new->prev = selectStart;
+
+        if(info->cursor != info->tail){
+            new->next = info->cursor->next;
+            new->next->prev = new;
+        }else{
+            info->tail = new;
+        }
     }else if(select<0){//cursor -> new -> selectStart的next
-        new->next = selectStart->next;
-        new->next->prev = new;
         info->cursor->next = new;
         new->prev = info->cursor;
+
+        if(selectStart != info->tail){
+            new->next = selectStart->next;
+            new->next->prev = new;
+        }else{
+            info->tail = new;
+        }
+        
     }
+
+    info->cursor = new;
+
 
     return info;
 }
@@ -89,70 +92,64 @@ int main(void){
     char *str=(char*)malloc(sizeof(char)*1000000);
     int letter,select;
     DoubleLL *selectStart=NULL;
-    bool selectionMode=0;
+    bool selectionMode=false;
 
-    
     
     for(int i=0;i<T;i++){
         scanf("%s",str);
 
         int j=0;
         while(str[j]!='\0'){
-
+            //printf("%c %d\n",str[j],select);
             letter=(int)str[j];
 
             if (islower(letter)){//a~z
                 DoubleLL *new=genDoubleLL(str[j]);
 
                 if(selectionMode){
-                    info=Overwrite(new,selectStart,info,select);
+                    if(select!=0)
+                        info=Overwrite(new,selectStart,info,select);
+                    else
+                        info=Insert(new,info);  
                     selectionMode=!selectionMode;//exit selectionMode
-                    select=0;//判斷select方向歸零
                 }else{
                     info=Insert(new,info);  
                 }
-
             }else{//指令
 
                 switch (str[j]){
 
                     case 'H'://左移
-                        if(selectionMode)
-                            select--;//選取模式往左
 
-                        if(info->cursor!=NULL){
-                            
-                            if(info->cursor==info->head)//已經在最頭
-                                info->cursor=NULL;
-                            else
-                                info->cursor=info->cursor->prev;
+                        if(info->cursor->letter != 'S'){//不在最頭
+                            info->cursor=info->cursor->prev;
+                            if(selectionMode)
+                                select--;//選取模式往左
                         }
                         break;
 
                     case 'L'://右移
-                        if(selectionMode)
-                            select++;//選取模式往右
 
-
-                        if(info->head!=NULL){
-                            if(info->cursor==NULL)//已經在最頭
-                                info->cursor = info->head;
-                            else if(info->cursor!=info->tail)//不在最尾
+                        if(info->cursor != info->tail){
                                 info->cursor=info->cursor->next;
-                        }
+                                if(selectionMode)
+                                    select++;//選取模式往右
+                            }
                         break;
 
                     case 'I'://移到最頭
-                        info->cursor=NULL;
-                        break;
+                        /*info->cursor=NULL;
+                        break;*/
 
                     case 'A'://移到最尾
-                        info->cursor=info->tail;
-                        break;
+                        /*info->cursor=info->tail;
+                        break;*/
 
                     case 'V'://SelectionMode
-                        if(selectionMode)//原本已經selectionMode,現在要關掉
+
+                        if(selectionMode){//原本已經selectionMode,現在要關掉
                             selectStart=NULL;
+                        }
                         else{//原本不在selectionMode,現在要進入
                             selectStart=info->cursor;
                             select=0;//紀錄選取的方向
@@ -174,7 +171,7 @@ int main(void){
             j++;
         }
 
-        DoubleLL *temp=info->head;
+        DoubleLL *temp=info->head->next;
         while(temp!=NULL){
             printf("%c",temp->letter);
             temp=temp->next;
