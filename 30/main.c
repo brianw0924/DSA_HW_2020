f74ee548a5f5910dfee93031f13828d7835f704c
brian <B06504102@ntu.edu.tw>
2020-05-26 10:56:39 +0800

    test

--- 30/main.c
+++ 30/main.c
@@ -18,81 +18,56 @@ int main(){
     int cur=0,*nextNegative=(int*)malloc(sizeof(int)*(N+1));
     long long int *hash =(long long int*)malloc(sizeof(long long int)*(N+1));
     hash[0] = 0;
-    hash[1] = Array[0];
     int i;
 
-    if(N>100000){
-
-
-
-    for(i=2;i<(N+1);i++){
+    for(i=1;i<(N+1);i++){
         hash[i] = hash[i-1] + Array[i-1];
-        if(Array[i-2]<0){
-            for(int j=cur;j<(i-2);j++)
-                nextNegative[j]=i-2;
-            cur = i-1;
+        if(Array[i-1]<0){
+            for(int j=cur;j<i;j++)
+                nextNegative[j]=i-1;
+            cur = i;
         }
-        // printf("%llu ",hash[i-1]);
     }
-    // printf("%llu\n",hash[N]);
-    for(int j=cur;j<(N-1);j++)
-        nextNegative[j]=N-1;
-    nextNegative[N-1] = N;
-    nextNegative[N] = N+1;
-    // for(int k=0;k<N;k++)
-    //     printf("nextNegative=%d\n",nextNegative[k]);
-    // for(int k=0;k<N;k++)
-    //     printf("%llu ",hash[k]);
-    // printf("%llu\n",hash[N]);
-
+    for(int j=cur;j<(N+1);j++)
+        nextNegative[j]=N+1;
+    // nextNegative[N-1] = N;
+    // nextNegative[N] = N+1;
     
     int l=0,r=1;
     while(l<N){
-        // printf("l = %d\n",l);
         while(r<(N+1)){
-            // printf("current l = %d, r = %d\n",l,r);
-            // if(hash[r]-hash[l] == K)
-            //     count++;
-            // r++;
-            if(hash[r]-hash[l] == K){
-                // printf("l=%d r=%d\n",l,r);
+            if(hash[r]-hash[l] == K){//hit
                 count++;
                 r = nextNegative[r];
-                // printf("next r = %d\n",r);
-            }else if(hash[r]-hash[l] < K)
+            }else if(hash[r]-hash[l] < K)//太小
                 r++;
-            else
+            else//太大
                 r = nextNegative[r];
         }
         l++;
         r=l+1;
     }
     printf("%d\n",count);
-    }else{
-        for(i=2;i<(N+1);i++)
-            hash[i] = hash[i-1] + Array[i-1];
-        // printf("%llu ",hash[i-1]);
-    }
-    // printf("%llu\n",hash[N]);
-    // for(int k=0;k<N;k++)
-    //     printf("nextNegative=%d\n",nextNegative[k]);
-    // for(int k=0;k<N;k++)
-    //     printf("%llu ",hash[k]);
-    // printf("%llu\n",hash[N]);
+
+    return 0;
+}
+
+
+    //     for(i=2;i<(N+1);i++)
+    //         hash[i] = hash[i-1] + Array[i-1];
+    // }
 
     
-    int l=0,r=1;
-    while(l<N){
-        // printf("l = %d\n",l);
-        while(r<(N+1)){
-            // printf("current l = %d, r = %d\n",l,r);
-            if(hash[r]-hash[l] == K)
-                count++;
-            r++;
-        }
-        l++;
-        r=l+1;
-    }
-    printf("%d\n",count);
+    // int l=0,r=1;
+    // while(l<N){
+    //     while(r<(N+1)){
+    //         if(hash[r]-hash[l] == K)
+    //             count++;
+    //         r++;
+    //     }
+    //     l++;
+    //     r=l+1;
+    // }
+    // printf("%d\n",count);
 
-    }
+    
