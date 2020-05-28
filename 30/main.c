1f784cf3d38453aaec2ea862d111728caf9cd78b
brian <B06504102@ntu.edu.tw>
2020-05-29 01:12:52 +0800

    test

--- 30/main.c
+++ 30/main.c
@@ -8,66 +8,126 @@
 #include <time.h>
 #include "akihabara.h"
 
+int hashSize = 1000000;//mod多少
+
+typedef struct node{
+    long long int key;
+    int count;
+    struct node *next;
+}Node;
+
+Node *getNode(){
+    Node *N=(Node*)malloc(sizeof(Node));
+    assert(N!=NULL);
+    N->count = 0;
+    N->next = NULL;
+    return N;
+}
+
+void hash(Node **Hash, int key){
+    int hashValue=key%hashSize;
+    Node *temp = Hash[hashValue];
+    if(temp->count == 0){//空櫃子
+        temp->count++;
+        temp->key = key;
+    }else{//Chaining
+        Node *prev=NULL;
+        while(temp->key != key){
+            prev = temp;
+            temp = temp->next;
+        }
+        if(temp == NULL){
+            prev->next = getNode();
+            temp = prev->next;
+            temp->key = key;
+            temp->count++;
+        }else{
+            temp->count++;
+        }
+    }
+}
+
+void init(Node **Hash, int size){
+    for(int i=0;i<size;i++){
+        // printf("%d\n",i);
+        Hash[i] = getNode();
+    }
+}
+
+int findTargetCount(Node **Hash, int target){
+    int hashValue = target%hashSize;
+    Node *temp = Hash[hashValue];
+    if(temp->count == 0){
+        // printf("test1\n");
+        return 0;
+    }
+    else{
+        while(temp->key != target)
+            temp = temp->next;
+        if(temp == NULL)
+            return 0;
+        else
+            return temp->count;
+    }
+}
+
+
+
 int Array[50000000];
 int main(){
     unsigned int N = getN();
     long long int K = getK();
     getArray(N, Array);
 
-    int count = 0;
-    int cur=0,*nextNegative=(int*)malloc(sizeof(int)*(N+1));
-    long long int *hash =(long long int*)malloc(sizeof(long long int)*(N+1));
-    hash[0] = 0;
-    int i;
+    // int hashSize = 10000000;//mod多少
+    int count=0;
+    long long int *Sum=(long long int*)malloc(sizeof(long long int)*N);
+    Node **negative_Hash=(Node**)malloc(sizeof(Node*)*hashSize);
+    Node **positive_Hash=(Node**)malloc(sizeof(Node*)*hashSize);
+    // int *negative_Hash=(int*)malloc(sizeof(int)*hashSize);
+    // int *positive_Hash=(int*)malloc(sizeof(int)*hashSize);
+    // printf("test1\n");
+    init(negative_Hash, hashSize);
+    init(positive_Hash, hashSize);
+    // printf("test2\n");
+    Sum[0]=Array[0];
+    for(int i=1;i<N;i++){
+        // printf("%llu\n",Sum[i-1]);
+        Sum[i] = Sum[i-1] + Array[i];
+    }
+    // printf("%llu\n",Sum[N-1]);
 
-    for(i=1;i<(N+1);i++){
-        hash[i] = hash[i-1] + Array[i-1];
-        if(Array[i-1]<0){
-            for(int j=cur;j<i;j++)
-                nextNegative[j]=i-1;
-            cur = i;
+
+    for(int i=0;i<N;i++){
+        int target = Sum[i]-K;
+        // printf("%d\n",target);
+
+        //找區間和=K
+        if(target<0){
+            target = -target;
+            count+=findTargetCount(negative_Hash,target);
+        }else if(target == 0){
+            count++;
+            count+=findTargetCount(positive_Hash,target);
+        }else
+            count+=findTargetCount(positive_Hash,target);
+
+
+        //處理key,Hash
+        if(Sum[i]<0){
+            int temp = -Sum[i];
+            hash(negative_Hash, temp);
+        }else{//Sum[i]>=0
+            hash(positive_Hash, Sum[i]);
         }
+
     }
-    for(int j=cur;j<(N+1);j++)
-        nextNegative[j]=N+1;
-    // nextNegative[N-1] = N;
-    // nextNegative[N] = N+1;
-    
-    int l=0,r=1;
-    while(l<N){
-        while(r<(N+1)){
-            if(hash[r]-hash[l] == K){//hit
-                count++;
-                r = nextNegative[r];
-            }else if(hash[r]-hash[l] < K)//太小
-                r++;
-            else//太大
-                r = nextNegative[r];
-        }
-        l++;
-        r=l+1;
-    }
+
     printf("%d\n",count);
+    
+
+
+
 
     return 0;
 }
-
-
-    //     for(i=2;i<(N+1);i++)
-    //         hash[i] = hash[i-1] + Array[i-1];
-    // }
-
-    
-    // int l=0,r=1;
-    // while(l<N){
-    //     while(r<(N+1)){
-    //         if(hash[r]-hash[l] == K)
-    //             count++;
-    //         r++;
-    //     }
-    //     l++;
-    //     r=l+1;
-    // }
-    // printf("%d\n",count);
-
-    
