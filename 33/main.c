9d03cb4a1a5a084ce7e2eb80fe8cded2740bfd0d
brian <B06504102@ntu.edu.tw>
2020-06-29 03:54:41 +0800

    33

--- 33/main.c
+++ 33/main.c
@@ -6,7 +6,7 @@
 #include <assert.h>
 #include <string.h>
 #include <time.h>
-// #pragma GCC optimize("O3,Ofast,no-stack-protector,unroll-loops,fast-math")
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4.1,sse4.2,avx,avx2,popcnt,tune=native")
 
 typedef struct listnode{
